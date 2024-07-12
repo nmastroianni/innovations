a114d6d38da1215c563957db6d48ed1e743f016e
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-07-12 11:45:38 -0400

    added leaders

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,18 +3,46 @@
  */
 export const innovations = [
   {
-    leader: 'FirstName LastName',
-    year: '2007',
-    innovation: 'Name of Innovation',
-    description: 'description of innovation and/or leader',
-    benefits: ['benefit 1', 'benefit 2'],
+    leader: 'Wilhelm Ritzerfeld',
+    year: '1923',
+    innovation: 'Spirit Duplicator (Ditto Machine)',
+    description:
+      'This device was known by several names, but produced duplicated materials with a unique look and smell. High-tech at the time, it allowed educators to produce many (though limited) copies of an original. Students could then have teacher customized content on which they could write.',
+    benefits: [
+      'Instructor customization',
+      'Consumable handouts',
+      'Facilitated repetition',
+    ],
+  },
+  {
+    leader: 'Roger Appledorn',
+    year: '1962',
+    innovation: 'Overhead Projector',
+    description:
+      'Loved or hated, this innovation was widely used in classrooms. Essentially, this device allowed teachers to move away from the chalkboard and closer to the students. Now teachers could be "with you" while they were teaching. Of course, the lights usually went off when the projector came on. This may have helped some while hurting others.',
+    benefits: [
+      'Instructor proximity',
+      'Facilitated student/teacher interactions during lessons',
+    ],
+  },
+  {
+    leader: 'Michael Sokolski',
+    year: '1972',
+    innovation: 'Scantron',
+    description:
+      'Loved or hated, this innovation was widely used in classrooms. Essentially, this device allowed teachers to move away from the chalkboard and closer to the students. Now teachers could be "with you" while they were teaching. Of course, the lights usually went off when the projector came on. This may have helped some while hurting others.',
+    benefits: [
+      'Instructor proximity',
+      'Facilitated student/teacher interactions during lessons',
+    ],
   },
 ]
 /**
  * Here you can create two introductory paragraphs if needed.
  */
 export const intro = {
-  paragraph1: '',
+  paragraph1:
+    'This journey through educational technology begins with the technologies I have encountered throughout my education and career.',
   paragraph2: '',
 }
 /**
@@ -22,6 +50,6 @@ export const intro = {
  */
 export const meta = {
   title: '',
-  description: '',
-  footer: '',
+  description: 'A look at educational technology leaders',
+  footer: 'A Project for TECH6373 by Neil Mastroianni',
 }
